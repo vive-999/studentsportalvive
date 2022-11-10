341e89fc79a5f9913c8261352747f58443d242d6
vivekrajp <vivekrajp@noc.softcell.com>
2022-11-10 18:12:50 +0530

    Added allowed host

--- student/settings.py
+++ student/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '(!$x4)xyxl&7c%u1eew(fkb9+__fgb2xh#+d_r4o@yyqo)p5q5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studentsportalvive.herokuapp.com',
+                '127.0.0.1']
 
 
 # Application definition
